d72cd5f1b2e707dc73be91dc9e49207a0594ca49
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-07-06 16:41:58 +0900

    ゲストログインの導入 #46
    user_sessions controllerを作成してゲストログイン機能を導入しました。ルーティングも追加。

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,12 @@
+class UserSessionsController < ApplicationController
+    def guest_login
+        @guest_user = User.create(
+        name: 'ゲスト',
+        email: SecureRandom.alphanumeric(10) + "@email.com",
+        password: 'password',
+        password_confirmation: 'password'
+        )
+        auto_login(@guest_user)
+        redirect_to root_path, success: 'ゲストとしてログインしました'
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#top"
+
+  post '/guest_login', to: 'user_sessions#guest_login'
 end
