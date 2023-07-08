fce99a2a54be17ed1f2dc51593e70ca4f8739630
itomiki35 <o.miki22@gmail.com>
2023-07-08 19:38:18 +0900

    refactor:google_login_apiコントローラー #70

--- app/controllers/google_login_api_controller.rb
+++ app/controllers/google_login_api_controller.rb
@@ -1,21 +1,47 @@
 class GoogleLoginApiController < ApplicationController
-  require 'googleauth/id_tokens/verifier'
+  require "googleauth/id_tokens/verifier"
 
   protect_from_forgery except: :callback
   before_action :verify_g_csrf_token
 
   def callback
-    payload = Google::Auth::IDTokens.verify_oidc(params[:credential], aud: ENV["GOOGLE_CLIENT_ID"])
-    user = User.find_or_create_by(email: payload['email'])
-    session[:user_id] = user.id
-    redirect_to quizzes_path, notice: 'ログインしました'
+    verify_token_and_redirect
   end
 
   private
 
+  def verify_token_and_redirect
+    payload = verify_oidc_token
+    user = find_or_create_user(payload)
+    user_session(user)
+    redirect_to quizzes_path, notice: t(".success")
+  end
+
+  def verify_oidc_token
+    Google::Auth::IDTokens.verify_oidc(params[:credential], aud: ENV.fetch("GOOGLE_CLIENT_ID"))
+  end
+
+  def find_or_create_user(payload)
+    User.find_or_create_by(email: payload["email"])
+  end
+
+  def user_session(user)
+    session[:user_id] = user.id
+  end
+
   def verify_g_csrf_token
-    if cookies["g_csrf_token"].blank? || params[:g_csrf_token].blank? || cookies["g_csrf_token"] != params[:g_csrf_token]
-      redirect_to root_path, notice: '不正なアクセスです'
-    end
+    redirect_to root_path, notice: t(".fail") if csrf_token_invalid?
+  end
+
+  def csrf_token_invalid?
+    missing_token? || invalid_token?
+  end
+
+  def missing_token?
+    cookies["g_csrf_token"].blank? || params[:g_csrf_token].blank?
+  end
+
+  def invalid_token?
+    cookies["g_csrf_token"] != params[:g_csrf_token]
   end
 end
