29bd1cc5e0bbe4afe8b20899e1ef14abab3dacf6
yuita-yoshihiko <soup998211@gmail.com>
2023-07-14 15:10:51 +0900

    クイズが作成できないバグの修正 #106
    テーブルをアクションテキストに対応した書き方に修正
    解説ページへのルーティングを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_022508) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_14_054138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,7 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_022508) do
     t.string "quiz_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "explanation", null: false
+    t.text "explanation"
     t.string "explanation_image"
     t.string "category", null: false
   end
