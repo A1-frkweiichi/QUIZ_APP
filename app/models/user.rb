8f1e2759f0d87eb5f88380ef6fa36fbd390db0f7
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-07-18 21:31:23 +0900

    rubocopの指摘修正 #14
    rubocopで指摘された箇所を修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-
+  # rubocop:disable Rails/HasManyOrHasOneDependent
   has_many :quizzes
+  # rubocop:enable Rails/HasManyOrHasOneDependent
   mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
