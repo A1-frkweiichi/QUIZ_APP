f4503b0fe4984ba5569d4f4ce6b76a0b919e7577
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-07-18 12:18:18 +0900

    画像アップロードに伴う変更 #14
    userテーブルにavaterカラム追加　アップロードされた画像を追跡対象外

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   has_many :quizzes
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
