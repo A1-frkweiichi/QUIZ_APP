1e2bbc3a1833dc6dda6dc417343a53239fee60ea
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-07-20 14:30:08 +0900

    マイページのArgumentErrorを修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,7 +14,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(_args)
+  def default_url(*args)
     "sample.jpg"
   end
   # For Rails 3.1+ asset pipeline compatibility:
