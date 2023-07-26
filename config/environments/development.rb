2293c674063ed38dafe5f4c434e46fd470eb3fd0
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-07-26 10:35:47 +0900

    本番環境のみ、:amazonに変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,9 +35,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
 
-  # MVP時に下のアクティブストレージの方向をlocalに戻す
-  #config.active_storage.service = :local
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
