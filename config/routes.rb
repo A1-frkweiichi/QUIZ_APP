5ac65f82878e7acbd3d03b9ed178535b25c8817f
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-08-03 01:11:24 +0900

    app/views/pages/配下ファイルにダークモード適応させたいが反映されない

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root "home#top"
+
+  resources:users, only: [:show, :edit, :update]
+
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks",
     registrations: 'users/registrations'
   }
-
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
@@ -14,10 +17,4 @@ Rails.application.routes.draw do
     end
   end
   get "explanation/:id", :to => "quizzes#explanation", as: 'explanation'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "home#top"
-
-  resources:users, only: [:show, :edit, :update]
 end
